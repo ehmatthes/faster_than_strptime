bd99dda08ca87ae53f7d95df0a52e378eff31e28
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-08-11 09:02:28 -0800

    String-based parsing.

--- process_data.py
+++ process_data.py
@@ -6,6 +6,15 @@ Timestamp format: "2016-02-09 15:45"
 from datetime import datetime
 
 
+def get_ts_simple_string_parser(line):
+    """Parse string through string slicing."""
+    year, month, day = int(line[:4]), int(line[5:7]), int(line[8:10])
+    hour, minute = int(line[11:13]), int(line[14:])
+
+    ts = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
+    return ts
+
+
 input_file = 'data_file.txt'
 
 print("Reading data from file...")
@@ -17,7 +26,10 @@ lines = [line.rstrip() for line in lines]
 print("Processing timestamps...")
 
 # Using strptime:
-timestamps = [datetime.strptime(line, "%Y-%m-%d %H:%M") for line in lines]
+# timestamps = [datetime.strptime(line, "%Y-%m-%d %H:%M") for line in lines]
+
+# Using manual string parsing:
+timestamps = [get_ts_simple_string_parser(line) for line in lines]
 
 
 """Verify ts objects."""
