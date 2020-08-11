8e9bb7010d134d2a624b9baff030e21ccdbb55b6
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-08-11 09:12:36 -0800

    Regex-based parsing.

--- process_data.py
+++ process_data.py
@@ -3,6 +3,7 @@
 Timestamp format: "2016-02-09 15:45"
 """
 
+import re
 from datetime import datetime
 
 
@@ -10,6 +11,16 @@ def get_ts_simple_string_parser(line):
     """Parse string through string slicing."""
     year, month, day = int(line[:4]), int(line[5:7]), int(line[8:10])
     hour, minute = int(line[11:13]), int(line[14:])
+
+    ts = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
+    return ts
+
+def get_ts_regex(line):
+    """Parse string using a regex."""
+    ts_pattern = re.compile('([\d]{4})-([\d]{2})-([\d]{2}) ([\d]{2}):([\d]{2})')
+    m = ts_pattern.match(line)
+    year, month, day = int(m.group(1)), int(m.group(2)), int(m.group(3))
+    hour, minute = int(m.group(4)), int(m.group(5))
 
     ts = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
     return ts
@@ -29,9 +40,12 @@ print("Processing timestamps...")
 # timestamps = [datetime.strptime(line, "%Y-%m-%d %H:%M") for line in lines]
 
 # Using manual string parsing:
-timestamps = [get_ts_simple_string_parser(line) for line in lines]
+# timestamps = [get_ts_simple_string_parser(line) for line in lines]
+
+# Using regex:
+timestamps = [get_ts_regex(line) for line in lines]
 
 
-"""Verify ts objects."""
+# Verify ts objects.
 for line, ts in zip(lines[:5], timestamps[:5]):
     print(line, ts.isoformat())
